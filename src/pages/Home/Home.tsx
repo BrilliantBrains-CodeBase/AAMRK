c8ac11fd3dee3292096c06ccf15560af74010596
Prashanth Naroju <prashanth.naro@gmail.com>
2026-03-23 00:17:21 +0530

    update the website with meta details

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,45 @@
-import LeadForm from "../../layout/lead form/LeadForm"
-import CTASection from "./components/CTASection"
-import GetLocation from "./components/GetLocation"
-import GetStarted from "./components/GetStarted"
-import Hero from "./components/Hero"
-import KeyAdvantages from "./components/KeyAdvantages"
-import WhatWeDo from "./components/WhatWeDo"
-import WhyChooseUs from "./components/WhyChooseUs"
+import { Helmet } from "react-helmet-async";
+
+import LeadForm from "../../layout/lead form/LeadForm";
+import CTASection from "./components/CTASection";
+import GetLocation from "./components/GetLocation";
+import GetStarted from "./components/GetStarted";
+import Hero from "./components/Hero";
+import KeyAdvantages from "./components/KeyAdvantages";
+import WhatWeDo from "./components/WhatWeDo";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 const Home = () => {
   return (
     <div>
+      {/* META */}
+      <Helmet>
+        <title>
+          AAMRK Solutions | Packaging, CAD, ERP & CNC Software Solutions
+        </title>
+
+        <meta
+          name="description"
+          content="AAMRK Solutions provides advanced CAD, ERP, RIP, and CNC solutions for the print and packaging industry. Streamline workflows, reduce costs, and boost productivity."
+        />
+
+        {/* Canonical */}
+        <link rel="canonical" href="https://aamrk.co.in/" />
+
+        {/* Open Graph */}
+        <meta property="og:title" content="AAMRK Solutions" />
+        <meta
+          property="og:description"
+          content="Smart software and machinery solutions for print and packaging industries."
+        />
+        <meta property="og:url" content="https://aamrk.co.in/" />
+        <meta property="og:type" content="website" />
+
+        {/* Optional but recommended */}
+        <meta name="robots" content="index, follow" />
+      </Helmet>
+
+      {/* PAGE CONTENT */}
       <Hero />
       <KeyAdvantages />
       <WhyChooseUs />
@@ -19,7 +49,7 @@ const Home = () => {
       <CTASection />
       <LeadForm />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
